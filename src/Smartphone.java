1f5e9334158a8901ef15c92f6f48f47c4d832486
pedro leonardo <peedroleonardolimalima2006@gmail.com>
2024-06-10 23:07:18 -0400

    Meu projeto iphone

---
+++ src/Smartphone.java
@@ -0,0 +1,71 @@
+
+    public class  Smartphone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+
+        public static void main(String[] args) {
+          
+
+           
+          
+        }
+
+        @Override
+        public void pausar() {
+        System.out.println("Pausando música");
+            
+        }
+
+        @Override
+        public void selecionarMusica() {
+            System.out.println("Selecionando música");
+            
+        }
+
+        @Override
+        public void tocar() {
+            System.out.println("Celular tocando");
+        }
+
+    
+        
+           
+
+        @Override
+        public void atender() {
+            System.out.println("Atendendo celular");
+            
+        }
+
+        @Override
+        public void iniciarCorreioVoz() {
+            System.out.println("Iniciando Correio de Voz");
+            
+        }
+
+        @Override
+        public void ligar() {
+            System.out.println("Ligando paraum celular");
+            
+        }
+
+        @Override
+        public void adicionarNovaAba() {
+            System.out.println("Abrindo nova aba");
+            
+        }
+
+        @Override
+        public void atualizarPagina() {
+           System.out.println("Atualizando página");
+            
+        }
+
+        @Override
+        public void exibirPagina() {
+            System.out.println("Exibindo página na web");
+            
+        }
+
+     
+        
+        
+}
